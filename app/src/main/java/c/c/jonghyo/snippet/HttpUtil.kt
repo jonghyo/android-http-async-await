a85eed2f8698275cfe0ea6bf0be173f1f0cf8452
jonghyo <m.73816686@gmail.com>
2019-02-16 17:13:28 +0900

    firest commit

---
+++ app/src/main/java/c/c/jonghyo/snippet/HttpUtil.kt
@@ -0,0 +1,17 @@
+package c.c.jonghyo.snippet
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+
+class HttpUtil {
+    fun httpGET1(url : String): String? {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+                .url(url)
+                .build()
+
+        val response = client.newCall(request).execute()
+        return response.body()?.string()
+    }
+}
---
+++ app/src/main/java/c/c/jonghyo/snippet/MainActivity.kt
@@ -0,0 +1,42 @@
+package c.c.jonghyo.snippet
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.eclipsesource.json.Json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    val URL = "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040"
+    var result = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val getButton = findViewById(R.id.button) as Button
+        getButton.setOnClickListener(object : View.OnClickListener {
+            override
+            fun onClick(view: View) {
+                onParallelGetButtonClick()
+            }
+        })
+    }
+
+    fun onParallelGetButtonClick() = GlobalScope.launch(Dispatchers.Main) {
+        val http = HttpUtil()
+        async(Dispatchers.Default) { http.httpGET1(URL) }.await().let {
+            val result = Json.parse(it).asObject()
+            val textView = findViewById(R.id.text) as TextView
+            textView.setText(result.get("description").asObject().get("text").asString())
+        }
+    }
+}
